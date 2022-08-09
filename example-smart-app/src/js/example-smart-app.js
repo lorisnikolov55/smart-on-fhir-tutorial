4d608405eec089a5c978c2ca7c1a92ff3e7f1688
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-08-09 13:17:02 -0500

    testing js file

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -33,6 +33,8 @@
 
           if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
+            console.log(patient.name[0]);
+            console.log(patient.name[0].family);
             lname = patient.name[0].family.join(' ');
           }
 
