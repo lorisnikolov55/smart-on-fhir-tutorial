7649f6046096d0d7a156f237c850bd8965dee6b0
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-08-09 13:45:41 -0500

    testing js file

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -32,10 +32,8 @@
           var lname = '';
 
           if (typeof patient.name[0] !== 'undefined') {
-            console.log(patient.name[0]);
-            console.log(patient.name[0].family);
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
+            fname = patient.name[0].given //.join(' ');
+            lname = patient.name[0].family //.join(' ');
           }
 
           var height = byCodes('8302-2');
