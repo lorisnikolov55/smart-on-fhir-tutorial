4b53c7bb4cfd61c80980f942d7237e30b3c5d979
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-08-16 12:07:14 -0500

    testing

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -134,7 +134,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
-    $('#hdl').html(p.immunization)
+    $('#hdl').html(p.immunization);
   };
 
 })(window);
