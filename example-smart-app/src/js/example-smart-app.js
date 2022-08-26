2d00f0a454ce55fca306c95653cc964801c28fb2
lorisnikolov55 <loris.nikolov@ucdconnect.ie>
2022-08-26 15:53:32 -0500

    parsing JSON

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -54,7 +54,7 @@
           .then((jsonData) => {
             console.log(jsonData);
             console.log(jsonData.entry[0].resource.manufacturer);
-            console.log(jsonData.entry[0].text.String(div));
+            //console.log(jsonData.entry[0].text.String(div));
             console.log(jsonData.entry[0].vaccineCode.text);
           })
           .catch((err) => {
